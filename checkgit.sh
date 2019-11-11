067f6d1b01c3b347e901bb0718f4f89ad4b37f2d
Mansubh Bhandari <bhandarimansubh@gmail.com>
2019-11-11 14:33:30 +1100

    first commit

---
+++ checkgit.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ ! -d ".git" ]];
+then
+    echo This is not a git project
+else
+    echo This is a git project
+fi;
+
+
---
+++ src/main/java/com/mansubh/Application.java
@@ -0,0 +1,5 @@
+package com.mansubh;
+
+public class Application {
+
+}
